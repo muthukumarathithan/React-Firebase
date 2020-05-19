2a1029a4bdb9998995797aeff5a7f0cf6f792e94
muthukumarathithan <37148241+muthukumarathithan@users.noreply.github.com>
2020-05-19 08:53:37 +0530

    dist

---
+++ dist/precache-manifest.4704b7db60758cf99133acad2f92ad30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3ca3c295eb1f04a18bf6ef2368ec3db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f753984dd2de3758630c",
+    "url": "/static/css/main.53aa6dca.chunk.css"
+  },
+  {
+    "revision": "2a316992c5bf1477e0bb",
+    "url": "/static/js/2.5ff0ef68.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/static/js/2.5ff0ef68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f753984dd2de3758630c",
+    "url": "/static/js/main.e28afef3.chunk.js"
+  },
+  {
+    "revision": "a606f28bf9a984c7ac3f",
+    "url": "/static/js/runtime-main.8e272fdd.js"
+  }
+]);
